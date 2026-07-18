247e27ea90de32d4590664f79f2ed7b3c0eac201
PDGamerSG <pallab4586das@gmail.com>
2026-07-19 01:48:44 +0530

    Make the trade page fit the viewport without scrolling

--- apps/web/app/trade/[market]/page.tsx
+++ apps/web/app/trade/[market]/page.tsx
@@ -7,11 +7,11 @@ import { useParams } from "next/navigation";
 
 export default function Page() {
     const { market } = useParams();
-    return <div className="flex flex-row flex-1">
-        <div className="flex flex-col flex-1">
+    return <div className="flex h-[calc(100dvh-3.5rem)] flex-row overflow-hidden">
+        <div className="flex min-w-0 flex-1 flex-col">
             <MarketBar market={market as string} />
-            <div className="flex h-[720px] flex-row border-b border-border">
-                <div className="flex flex-col flex-1">
+            <div className="flex min-h-0 flex-1 flex-row">
+                <div className="flex min-w-0 flex-1 flex-col">
                     <TradeView market={market as string} />
                 </div>
                 <div className="flex w-[250px] flex-col overflow-hidden border-l border-border">
@@ -19,7 +19,7 @@ export default function Page() {
                 </div>
             </div>
         </div>
-        <div className="w-[280px] flex-none border-l border-border">
+        <div className="w-[280px] flex-none overflow-y-auto border-l border-border no-scrollbar">
             <SwapUI market={market as string} />
         </div>
     </div>
